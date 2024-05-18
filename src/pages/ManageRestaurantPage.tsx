f679cecde9bdf18bcb85f7e6e7b17f1b7e0e95fe
afaqrehman98 <afaqrehman98@gmail.com>
2024-05-18 10:18:30 +0200

    User Profile form featured added

---
+++ src/pages/ManageRestaurantPage.tsx
@@ -0,0 +1,27 @@
+import {
+  useCreateMyRestaurant,
+  useGetMyRestaurant,
+  useUpdateMyRestaurant,
+} from "@/api/MyRestaurantApi";
+import ManageRestaurantForm from "@/forms/user-profile-form/manage-restaurant-form/ManageRestaurantForm";
+
+const ManageRestaurantPage = () => {
+  const { createRestaurant, isLoading: iscreateLoading } =
+    useCreateMyRestaurant();
+
+  const { restaurant } = useGetMyRestaurant();
+
+  const { updateRestaurant, isLoading: isUpdateLoading } =
+    useUpdateMyRestaurant();
+
+  const isEditing = !!restaurant;
+  return (
+    <ManageRestaurantForm
+      restaurant={restaurant}
+      onSave={isEditing ? updateRestaurant : createRestaurant}
+      isLoading={iscreateLoading || isUpdateLoading}
+    />
+  );
+};
+
+export default ManageRestaurantPage;
